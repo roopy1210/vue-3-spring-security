17c41bd25eb9d7bc641e31edb9359463f0faed40
roopy1210 <39702082+roopy1210@users.noreply.github.com>
2021-12-05 23:22:51 +0900

    팩키지변경

--- api-server/src/main/java/com/roopy/ApiServerApplication.java
+++ api-server/src/main/java/com/roopy/ApiServerApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -27,6 +30,19 @@ public class ApiServerApplication {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.roopy.api"))
-                .build();
+                .build()
+                .apiInfo(apiDetails());
+    }
+
+    private ApiInfo apiDetails() {
+        return new ApiInfo(
+                "Front-end Application API",
+                "로그인, 사용자생성, 권한 API",
+                "1.0",
+                "Free to use",
+                new springfox.documentation.service.Contact("Roopy", "http://roopy","swjeong1210@gmail.com"),
+                "Free License",
+                "http://roopy",
+                Collections.emptyList());
     }
 }
--- auth-server/src/main/java/com/roopy/AuthServerApplication.java
+++ auth-server/src/main/java/com/roopy/AuthServerApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class AuthServerApplication {
@@ -19,6 +22,19 @@ public class AuthServerApplication {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.roopy.auth"))
-                .build();
+                .build()
+                .apiInfo(apiDetails());
+    }
+
+    private ApiInfo apiDetails() {
+        return new ApiInfo(
+                "JWT Token Manage And User Manage API",
+                "토큰 관리 및 사용자 관리 API",
+                "1.0",
+                "Free to use",
+                new springfox.documentation.service.Contact("Roopy", "http://roopy","swjeong1210@gmail.com"),
+                "Free License",
+                "http://roopy",
+                Collections.emptyList());
     }
 }
